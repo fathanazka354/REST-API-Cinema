b8db053332108c63c79dc3d26d4276cf6ca4c8b4
Fathan azka <fazka313@gmail.com>
2022-10-16 13:45:47 +0700

    implementation of Jasper on Project

--- src/main/java/com/binar/cinema/service/InvoiceService.java
+++ src/main/java/com/binar/cinema/service/InvoiceService.java
@@ -1,7 +1,9 @@
 package com.binar.cinema.service;
 
+import com.binar.cinema.dto.Invoice;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface InvoiceService {
+    Invoice getInvoiceById(Long orderId);
 }
