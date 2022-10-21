b8b5f363e7186246d24b419b50430478994439d0
Fathan azka <fazka313@gmail.com>
2022-10-21 22:30:43 +0700

    update SecurityConfig.Java

--- src/main/java/com/binar/cinema/security/SecurityConfig.java
+++ src/main/java/com/binar/cinema/security/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import java.util.List;
 
 import static com.binar.cinema.security.SecurityConstant.*;
 
