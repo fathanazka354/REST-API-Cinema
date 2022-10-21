ab8ffe750acb29bd5fbadef49e112c094542070b
Fathan azka <fazka313@gmail.com>
2022-10-21 23:44:00 +0700

    menerapkan corsConfigurationSource pada SecuritySource.Java

--- src/main/java/com/binar/cinema/security/SecurityConfig.java
+++ src/main/java/com/binar/cinema/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.binar.cinema.security.manager.CustomAuthenticationManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -15,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static com.binar.cinema.security.SecurityConstant.*;
 
@@ -49,11 +53,11 @@ public class SecurityConfig {
 //                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 //        Way 2
-        http.cors().and().csrf().disable().antMatcher("/swagger-ui/**").authorizeRequests()
+        http.csrf().disable().cors().configurationSource(configurationSource()).and()
+                .antMatcher("/swagger-ui/**").authorizeRequests()
                 .antMatchers(EMPLOYEE_PATH).authenticated()
                 .antMatchers("/swagger-ui/**").permitAll()
-                .antMatchers(CUSTOMER_PATH, MOVIE_PATH).hasRole("ADMIN")
-                .anyRequest().authenticated().and()
+                .antMatchers(CUSTOMER_PATH, MOVIE_PATH).hasRole("ADMIN").and()
                 .userDetailsService(users())
                 .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
                 .addFilter(authenticationFilter)
@@ -61,6 +65,21 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().httpBasic();
         return http.build();
     }
+
+    private CorsConfigurationSource configurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("X-Requested-With");
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.PUT);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Bean
     public UserDetailsService users(){
         UserDetails admin = User.builder()
