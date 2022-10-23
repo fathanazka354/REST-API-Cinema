458bc537d1f40d5acf2db10217f5ef11ee7ff2b9
Fathan azka <fazka313@gmail.com>
2022-10-23 14:56:43 +0700

    menghapus method pada CustomerController.Java

--- src/main/java/com/binar/cinema/controller/CustomerController.java
+++ src/main/java/com/binar/cinema/controller/CustomerController.java
@@ -13,8 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
-@CrossOrigin(origins = {"https://rest-api-cinema-production.up.railway.app", "http://rest-api-cinema-production.up.railway.app", "https://rest-api-cinema-production.up.railway.app/swagger-ui/index.html#"},
-methods = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT})
+@CrossOrigin(origins = {"https://rest-api-cinema-production.up.railway.app", "http://rest-api-cinema-production.up.railway.app", "https://rest-api-cinema-production.up.railway.app/swagger-ui/index.html#"})
 public class CustomerController {
     @Autowired
     CustomerService customerService;
